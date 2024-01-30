4b8451afb0eb303232fc16c44e41cb0670f245af
Jdearman2204 <155987822+Jdearman2204@users.noreply.github.com>
2024-01-30 08:49:03 -0600

    Client Side Programming 1/30/24

--- ClientSide/Chapter02/Chapter02/chapter/js02.js
+++ ClientSide/Chapter02/Chapter02/chapter/js02.js
@@ -11,6 +11,11 @@
  */
 
 //setup the form when the page loads
+//declare global constants for the application
+const EMP_COST = 100;  //cost of the photographers per hour
+const BOOK_COST = 350;  //cost of memory book
+const REPRO_COST = 1250;  //cost of reproduction rights
+const TRAVEL_COST = 2;  //cost of travel per mile
 window.addEventListener("load",setupForm);
 
 //set the form's default values
---
+++ ClientSide/Chapter02/Chapter02/team/ch2Snippets.js
@@ -0,0 +1,13 @@
+let user = "Dawson";    //global scope - global variable
+
+function showUser()
+{
+    let User = "Reynolds";  //local scope - local variable
+    console.log("In showUser - local scope");
+    console.log(user);
+}
+
+
+console.log("Global scope");
+console.log(user);  //print the global variable user
+showUser();
