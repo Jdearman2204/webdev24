a67186d1632e32d0846ae39c895c24ef51b7c978
Jdearman2204 <155987822+Jdearman2204@users.noreply.github.com>
2024-02-06 09:30:40 -0600

    Working?

---
+++ ClientSide/Chapter02/Chapter02/project01/project02-01.js
@@ -0,0 +1,19 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Project 02-01
+
+      Celsius <-> Farenheit Coverter
+      Author: 
+      Date:   
+
+      Filename: project02-01.js
+ */
+
+function FarenheitToCelcius(degree) {
+      return (degree-32) * (degree/1.8);
+}
+
+function CelsiusToFarenheit(degree) {
+      return (degree*1.8) * (degree+32);
+}
+
---
+++ ClientSide/Chapter02/Chapter02/project03/project02-03.js
@@ -0,0 +1,14 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Project 02-03
+
+      Application to return the shape of a clicked object
+      Author: 
+      Date:   
+
+      Filename: project02-03.js
+ */
+
+document.getElementById("square").onmouseover = function() {
+      document.getElementById("feedback").innerHTML = ""
+}
