7651e058ed8a1843745c58f595380491e20caae3
Jdearman2204 <155987822+Jdearman2204@users.noreply.github.com>
2024-03-21 08:54:19 -0500

    5-1

--- ClientSide/Chapter05/project01/project05-01.js
+++ ClientSide/Chapter05/project01/project05-01.js
@@ -4,8 +4,20 @@
       Project 05-01
 
       Project to present an online quiz with a countdown clock
-      Author: 
-      Date:   
+      Author: Jacob Dearman
+      Date:   3/19/24
+
+      Filename: project05-01.js
+*/
+
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 5
+      Project 05-01
+
+      Project to present an online quiz with a countdown clock
+      Author: Jacob Dearman
+      Date:   3/19/24
 
       Filename: project05-01.js
 */
@@ -14,44 +26,43 @@
 // and the correct answers to each quiz question
 const quizTime = 20;
 const correctAnswers = ["10", "4", "-6", "5", "-7"];
-let timeID;
 
 // Elements in the quiz page
 let startQuiz = document.getElementById("startquiz");
-startQuiz.onclick = function () {
-   overlay.setAttribute("class", "showQuiz");
+let quizClock = document.getElementById("quizclock");
+let overlay = document.getElementById("overlay");
+let timeLeft = quizTime
+
+// Declare the ID for timed commands
+let timeID;
+// Declare the node list for questions
+let questionList = document.querySelectorAll("div#quiz input");
+
+// Add an onclick event handler to the startQuiz object
+startQuiz.onclick = function() {
+   overlay.className = "showquiz";
    timeID = setInterval(countdown, 1000);
 }
-let quizClock = document.getElementById("quizclock");
+
+// Countdown function to update the quiz clock
 function countdown() {
-   let totalCorrect = checkAnswers();
    if (timeLeft === 0) {
       clearInterval(timeID);
+      let totalCorrect = checkAnswers();
+
+      if (totalCorrect === correctAnswers.length) {
+         alert("Congratulations! You got 100%!")
+      } else {
+         alert("You got " + (correctAnswers.length - totalCorrect) + " out of " + correctAnswers.length);
+         timeLeft = quizTime;
+         quizClock.value = timeLeft;
+         overlay.className = "hidequiz";
+      }
    } else {
-      timeLeft = quizTime;
-      quizClock.value = timeLeft;
-      overlay.className = "hidequiz";
       timeLeft--;
       quizClock.value = timeLeft;
    }
-   if (totalCorrect === correctAnswers.length) {
-      alert("Congratulations. You got 100%")
-   } else {
-      alert("You got " + (questionList.length - totalCorrect) + " out of " + questionList.length);
-   }
 }
-let overlay = document.getElementById("overlay");
-let questionList = document.querySelectorAll("div#quiz input");
-
-// Initialize the quiz time
-quizClock.value = quizTime;
-let timeLeft = quizTime;
-
-// Declare the ID for timed commands
-// and the node list for questions
-
-
-
 
 
 
@@ -73,14 +84,14 @@ let timeLeft = quizTime;
 /*------------- Function to check the student answers ----------------*/
 function checkAnswers() {
    let correctCount = 0;
-
+   
    for (let i = 0; i < questionList.length; i++) {
       if (questionList[i].value === correctAnswers[i]) {
          correctCount++;
          questionList[i].className = "";
       } else {
          questionList[i].className = "wronganswer";
-      }
+      }      
    }
    return correctCount;
 }
