a873a6bbd71e49f783f61b861a7ed6679498ab60
Jdearman2204 <155987822+Jdearman2204@users.noreply.github.com>
2024-03-28 08:59:10 -0500

    Ch6

--- ClientSide/Chapter06/project01/project06-01.js
+++ ClientSide/Chapter06/project01/project06-01.js
@@ -15,11 +15,12 @@ let pwd = document.getElementById("pwd");
 let pwd2 = document.getElementById("pwd2");
 
 submitButton.addEventListener("click", function() {
-      if {
-
-      } else if {
-
+      if (pwd.validity.patternMismatch) {
+            pwd.setCustomValidity("Your password must be at least 8 characters with at least one letter and one number");
+      } else if (pwd.value != pwd2.value) {
+            pwd2.setCustomValidity("Your passwords must match");
       } else {
-            
+            pwd.setCustomValidity("");
       }
 });
+
